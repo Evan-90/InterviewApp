90e670f8ab70d9ff207486e6870e417259c06080
evan <993523104@qq.com>
2018-01-12 17:12:40 +0800

    UI: login page && register page

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { List, InputItem, Radio, WingBlank, WhiteSpace, Button } from 'antd-mobile'
+import Logo from "../../component/logo/logo";
+
 
 class Register extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: 'genius' // 或者boss
+    }
+    this.login = this.login.bind(this)
+  }
+  login() {
+    this.props.history.push('/login')
+  }
   render() {
-    return <h2>注册页</h2>
+    const RadioItem = Radio.RadioItem
+    return (
+      <div>
+        <Logo/>
+        <h2>注册页面</h2>
+        <WingBlank>
+          <List>
+            <WhiteSpace/>
+            <InputItem>用户名</InputItem>
+            <WhiteSpace/>
+            <InputItem>密码</InputItem>
+            <WhiteSpace/>
+            <InputItem>确认密码</InputItem>
+            <WhiteSpace/>
+            <RadioItem checked={this.state.type == 'genius'} >牛人</RadioItem>
+            <WhiteSpace/>
+            <RadioItem checked={this.state.type == 'boss'} >BOSS</RadioItem>
+          </List>
+          <WhiteSpace/>
+          <Button type="primary">注册</Button>
+          <WhiteSpace/>
+          <Button type="primary" onClick={this.login}>去登录</Button>
+        </WingBlank>
+      </div>
+    )
   }
 }
 
