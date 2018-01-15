5b38ca24e135b42e7363e0efe82879c07a57de07
evan <993523104@qq.com>
2018-01-15 10:21:38 +0800

    to redirect after register

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
+import { getRedirectPath } from "../util/util"
+import {Toast} from 'antd-mobile'
 
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 const ERROR_MDG = 'ERROR_MDG'
 const initState = {
+  // 页面跳转路径
+  redirectTo: '',
+  // 是否登录
   isAuth: false,
+  // 用户信息
   user: '',
   pwd: '',
   type: ''
@@ -12,7 +18,7 @@ const initState = {
 export function user(state = initState, action) {
   switch (action.type){
     case REGISTER_SUCCESS:
-      return {...state, msg: '', isAuth: true, ...action.payload}
+      return {...state, msg: '', redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
     case ERROR_MDG:
       return {...state, msg: action.msg, isAuth: false}
     default:
@@ -24,6 +30,8 @@ function registerSuccess(data) {
   return {type: REGISTER_SUCCESS, payload: data}
 }
 function errorMsg(msg) {
+  // 提示错误信息
+  Toast.info(msg, 2)
   return {msg, type: ERROR_MDG}
 }
 export function register({user, pwd, repeatpwd, type}) {
@@ -38,7 +46,7 @@ export function register({user, pwd, repeatpwd, type}) {
       if(res.status === 200 && res.data.code === 0){
         dispatch(registerSuccess({user, pwd, type}))
       }else {
-        dispatch(errorMsg(res.date.msg))
+        dispatch(errorMsg(res.data.msg))
       }
     })
   }
